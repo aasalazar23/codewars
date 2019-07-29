3c43abc1c5f34d1dbbcbdb0f949778093451f809
Alejandro <aasalazar23@gmail.com>
2019-07-29 11:04:22 -0600

    check for digits 1-9 columns, rows sudoku

--- js/Sudoku.js
+++ js/Sudoku.js
@@ -2,6 +2,8 @@
  * Write a function validSolution/ValidateSolution/valid_solution() that accepts a 2D array representing a Sudoku board,
  * and returns true if it is a valid solution, or false otherwise. The cells of the sudoku board may also contain 0's,
  * which will represent empty cells. Boards containing one or more zeroes are considered to be invalid solutions.
+ * 
+ * https://www.codewars.com/kata/529bf0e9bdf7657179000008/train/javascript
  */
 
  function validSolution(board) {
